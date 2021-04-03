1ffb1dca3204bb6c9af0f02454122dee08d2017c
ofadiman <ofadiman@gmail.com>
2021-04-03 13:03:32 +0200

    fix: handle error during directory read (#12)
    - Remove pre-commit hook from husky.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
+/**
+ * For local testing execute `echo "potential commit message" | commitlint`.
+ */
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Disable validation for max body/header length.
+    'body-max-line-length': [0],
+    'header-max-length': [0]
+  }
 }
