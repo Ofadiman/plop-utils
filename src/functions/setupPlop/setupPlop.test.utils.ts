cb21df48a37569ea3a5c5ba323354675ece19ca5
ofadiman <ofadiman@gmail.com>
2021-04-04 11:00:39 +0200

    refactor: handle built-in generators in setupPlop function (#18)

--- src/functions/setupPlop/setupPlop.test.utils.ts
+++ src/functions/setupPlop/setupPlop.test.utils.ts
@@ -1,5 +1,9 @@
 import getPlop from 'node-plop'
 
+import * as plopGeneratorModule from '../../generators/plop-generator/plop-generator.setup'
+
+export const setupCustomGeneratorSpy = jest.spyOn(plopGeneratorModule, 'setupCustomGenerator')
+
 export const setGeneratorMock = jest.fn().mockName('setGeneratorMock')
 export const setWelcomeMessageMock = jest.fn().mockName('setWelcomeMessageMock')
 export const setHelperMock = jest.fn().mockName('setHelperMock')
