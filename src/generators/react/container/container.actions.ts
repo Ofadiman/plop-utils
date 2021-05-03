de02339f1950ea908b2f337b32afb96148f8ae8a
Ofadiman <ofadiman@gmail.com>
2021-05-03 16:26:45 +0200

    fix: container generator root path

--- src/generators/react/container/container.actions.ts
+++ src/generators/react/container/container.actions.ts
@@ -1,35 +1,37 @@
 import { containerConstants } from './container.constants'
-import { GetContainerActionsReturn } from './container.types'
+import { GetContainerActionsOptions, GetContainerActionsReturn } from './container.types'
 
 const { handlebars } = containerConstants
 
-export const getContainerActions = (): GetContainerActionsReturn => {
+export const getContainerActions = (options: GetContainerActionsOptions): GetContainerActionsReturn => {
+  const { root } = options
+
   return {
     javascript: {
       main: {
-        path: `src/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.jsx`,
+        path: `${root}/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.jsx`,
         template: require('./templates/javascript/[name].container.template').template,
         type: 'add'
       },
       tests: {
-        path: `src/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.test.jsx`,
+        path: `${root}/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.test.jsx`,
         template: require('./templates/javascript/[name].container.test.template').template,
         type: 'add'
       }
     },
     typescript: {
       main: {
-        path: `src/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.tsx`,
+        path: `${root}/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.tsx`,
         template: require('./templates/typescript/[name].container.template').template,
         type: 'add'
       },
       tests: {
-        path: `src/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.test.tsx`,
+        path: `${root}/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.test.tsx`,
         template: require('./templates/typescript/[name].container.test.template').template,
         type: 'add'
       },
       types: {
-        path: `src/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.types.ts`,
+        path: `${root}/containers/${handlebars.name.pascalCase}/${handlebars.name.pascalCase}.container.types.ts`,
         template: require('./templates/typescript/[name].container.types.template').template,
         type: 'add'
       }
